8a6023ade6caeafe4ea852a1e7c69fccc5add20e
Ethan Suttner <ethan.suttner@gmail.com>
2016-04-11 21:43:17 -0400

    Created mission and formatted pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "/contact" => "pages#contact"
+  get "/mission" => "pages#mission"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
