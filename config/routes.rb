411672a93963506a416159f6dd94215f469db09a
Ethan Suttner <ethan.suttner@gmail.com>
2016-04-18 22:22:34 -0400

    added glyphicons and updated routes. broke pinteresting logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :pins
+  
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
-  get "/contact" => "pages#contact"
   get "/mission" => "pages#mission"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
